623acf9350d05c35829caa5de0a494e4ab179169
DizzyDose <97802965+DizzyDose@users.noreply.github.com>
2025-09-23 23:17:44 +0200

    update, V1.0 - nonnegative

--- CS/consoleCalculator/src/main.cs
+++ CS/consoleCalculator/src/main.cs
@@ -5,22 +5,22 @@ using System.Threading;
 public class Program
 {
 	public static void Main(string[] args)
-	{		
+	{
 		ConsoleSetup();
 
 		List<string> TokenList = new List<string>();
-		
+
 		while (true)
 		{
 			Console.WriteLine("Console Calculator, please write the expression below to be calculated:");
 			string inputStream = Console.ReadLine();
 			if (inputStream != null)
-				TokenList = Tokenize(inputStream);
-			foreach (string token in TokenList)
-				Console.WriteLine(token);
+				TokenList = Tokenize(inputStream);			
 			if (CheckSyntax(TokenList))
-				Console.WriteLine(ExpressionCalculate(TokenList, 0, TokenList.Count));
-			else {
+				Console.WriteLine(ExpressionCalculate(TokenList));
+			
+			else
+			{
 				Console.WriteLine("Syntax error");
 			}
 		}
@@ -60,18 +60,18 @@ public class Program
 	{
 		string temp = "";
 		List<char> tokenList = new List<char>();
-		
-		foreach(char character in inputStream)
+
+		foreach (char character in inputStream)
 		{
-			if(character == 32)
+			if (character == 32)
 				continue;
 			tokenList.Add(character);
 		}
-		
+
 		List<string> tokenListConCat = new List<string>();
-		foreach(char character in tokenList)
+		foreach (char character in tokenList)
 		{
-			switch(character)
+			switch (character)
 			{
 				case '+':
 				case '-':
@@ -84,38 +84,183 @@ public class Program
 				case '[':
 				case ']':
 				case '^':
-					if(temp != "") tokenListConCat.Add(temp);
+					if (temp != "") tokenListConCat.Add(temp);
 					temp = "";
 					tokenListConCat.Add(character.ToString());
 					break;
 				default:
 					temp += character.ToString();
 					break;
-				
+
 			}
 		}
 		tokenListConCat.Add(temp); //add last missing token
-		
+
 		return tokenListConCat;
 	}
-	public static double ExpressionCalculate(List<string> tokenizedList, int startIndex, int lastIndex)
+	public static double ExpressionCalculate(List<string> tokenizedList)
 	{
 		double result = 0;
 
 		//
+		int[] PriorityTokenPositions = SearchForPriorityTokens(tokenizedList);
+		int[] OperandPositions = SearchForOperands(tokenizedList);
+		int pos1 = 0; int pos2 = 0;
+		double temp;
+		while (tokenizedList.Count != 1)
+		{
+			for (int i = 0; i < tokenizedList.Count; i++)
+			{
+				if (PriorityTokenPositions[i] == 3)
+				{
+					pos1 = i;
+				}
+				if (PriorityTokenPositions[i] == 4)
+				{
+					pos2 = i;
+					break;
+				}
+			}
 
-		int[] positions = SearchForPriorityTokens(tokenizedList);
+			if (pos1 == pos2) // no brackets
+			{
+				for (int i = tokenizedList.Count - 1; i > 0; i--)
+				{
+					if (PriorityTokenPositions[i] == 2)
+					{
+						temp = Exponent(Convert.ToDouble(tokenizedList[i - 1]), Convert.ToDouble(tokenizedList[i + 1]));
+						RemoveAndUpdate(temp, ref tokenizedList, i, ref PriorityTokenPositions, ref OperandPositions);
+					}
+				}
+				OperandPositions = SearchForOperands(tokenizedList);
+				for (int i = 0; i < tokenizedList.Count; i++)
+				{
+					if (PriorityTokenPositions[i] == 1)
+					{
+						switch (OperandPositions[i])
+						{
+							case 3:
+								temp = Multiply(Convert.ToDouble(tokenizedList[i - 1]), Convert.ToDouble(tokenizedList[i + 1]));
+								RemoveAndUpdate(temp, ref tokenizedList, i, ref PriorityTokenPositions, ref OperandPositions);
+								break;
+							case 4:
+								temp = Divide(Convert.ToDouble(tokenizedList[i - 1]), Convert.ToDouble(tokenizedList[i + 1]));
+								RemoveAndUpdate(temp, ref tokenizedList, i, ref PriorityTokenPositions, ref OperandPositions);
+								break;
+						}
+					}
+				}
+				OperandPositions = SearchForOperands(tokenizedList);
+				for (int i = 0; i < tokenizedList.Count; i++)
+				{
+					switch (OperandPositions[i])
+					{
+						case 1:
+							temp = Add(Convert.ToDouble(tokenizedList[i - 1]), Convert.ToDouble(tokenizedList[i + 1]));
+							RemoveAndUpdate(temp, ref tokenizedList, i, ref PriorityTokenPositions, ref OperandPositions);
+							break;
+						case 2:
+							temp = Subtract(Convert.ToDouble(tokenizedList[i - 1]), Convert.ToDouble(tokenizedList[i + 1]));
+							RemoveAndUpdate(temp, ref tokenizedList, i, ref PriorityTokenPositions, ref OperandPositions);
+							break;
+					}
+				}
+			}
+			else // with brackets
+			{				
+				for (int i = pos2 - 1; i > pos1; i--)
+				{
+					if (PriorityTokenPositions[i] == 2)
+					{
+						temp = Exponent(Convert.ToDouble(tokenizedList[i - 1]), Convert.ToDouble(tokenizedList[i + 1]));
+						RemoveAndUpdate(temp, ref tokenizedList, i, ref PriorityTokenPositions, ref OperandPositions);
+					}
+				}
+				OperandPositions = SearchForOperands(tokenizedList);
+				for (int i = pos1; i < pos2; i++)
+				{
+					if (PriorityTokenPositions[i] == 1)
+					{
+						switch (OperandPositions[i])
+						{
+							case 3:
+								temp = Multiply(Convert.ToDouble(tokenizedList[i - 1]), Convert.ToDouble(tokenizedList[i + 1]));
+								RemoveAndUpdate(temp, ref tokenizedList, i, ref PriorityTokenPositions, ref OperandPositions);
+								break;
+							case 4:
+								temp = Divide(Convert.ToDouble(tokenizedList[i - 1]), Convert.ToDouble(tokenizedList[i + 1]));
+								RemoveAndUpdate(temp, ref tokenizedList, i, ref PriorityTokenPositions, ref OperandPositions);
+								break;
+						}
+					}
+				}
+				OperandPositions = SearchForOperands(tokenizedList);
+				for (int i = pos1; i < pos2; i++)
+				{
+					switch (OperandPositions[i])
+					{
+						case 1:
+							temp = Add(Convert.ToDouble(tokenizedList[i - 1]), Convert.ToDouble(tokenizedList[i + 1]));
+							RemoveAndUpdate(temp, ref tokenizedList, i, ref PriorityTokenPositions, ref OperandPositions);
+							break;
+						case 2:
+							temp = Subtract(Convert.ToDouble(tokenizedList[i - 1]), Convert.ToDouble(tokenizedList[i + 1]));
+							RemoveAndUpdate(temp, ref tokenizedList, i, ref PriorityTokenPositions, ref OperandPositions);
+							break;
+					}
+				}
+				for (int i = 0; i < tokenizedList.Count; i++)
+				{
+				    if (PriorityTokenPositions[i] == 3)
+				    {
+				        pos1 = i;
+				    }
+				    if (PriorityTokenPositions[i] == 4)
+				    {
+				        pos2 = i;
+				        break;
+				    }
+				}
+				RemoveAndUpdateBrackets(ref tokenizedList, ref PriorityTokenPositions, ref OperandPositions, pos1, pos2);			
+				pos1 = 0; pos2 = 0;
+			} 
+		}
+		result = Convert.ToDouble(tokenizedList[0]);
 
-		
-		
 		//
 
 		return result;
 	}
+	public static int[] SearchForOperands(List<string> tokenizedList)
+	{
+		int[] OperandPositions = new int[tokenizedList.Count];
+		for (int i = 0; i < tokenizedList.Count; i++)
+		{
+			switch (tokenizedList[i])
+			{
+				case "+":
+					OperandPositions[i] = 1;
+					break;
+				case "-":
+					OperandPositions[i] = 2;
+					break;
+				case "*":
+					OperandPositions[i] = 3;
+					break;
+				case "/":
+					OperandPositions[i] = 4;
+					break;
+				case "^":
+					OperandPositions[i] = 5;
+					break;
+			}
+		}
+		return OperandPositions;
+	}
 	public static int[] SearchForPairedTokens(List<string> tokenizedList)
 	{
-		int[] positions = new int[tokenizedList.Count+1]; for (int i = 0; i< tokenizedList.Count; i++) { positions[i] = 0; } 
-		string[] keys = { "(", "[", "{" , ")", "]", "}" }; //1, 2, 3, 4, 5, 6
+		int[] positions = new int[tokenizedList.Count + 1]; for (int i = 0; i < tokenizedList.Count; i++) { positions[i] = 0; }
+		string[] keys = { "(", "[", "{", ")", "]", "}" }; //1, 2, 3, 4, 5, 6
 		int[] keyPairs = new int[6];
 		for (int i = 0; i < tokenizedList.Count; i++)
 		{
@@ -127,18 +272,6 @@ public class Program
 		if (keyPairs[0] != keyPairs[3]) { Console.WriteLine("Syntax error, missing )?"); positions[positions.Length - 1] = -1; }
 		if (keyPairs[1] != keyPairs[4]) { Console.WriteLine("Syntax error, missing ]?"); positions[positions.Length - 1] = -1; }
 		if (keyPairs[2] != keyPairs[5]) { Console.WriteLine("Syntax error, missing }?"); positions[positions.Length - 1] = -1; }
-
-		//
-
-		for (int i = 0; i < positions.Length; i++)
-		{
-			if (positions[i] != 0)
-			{
-				Console.WriteLine(i + " " + positions[i]);
-			}
-		}
-
-		//
 
 		return positions;
 	}
@@ -167,10 +300,12 @@ public class Program
 				case 1:
 				case 2:
 				case 3:
+					PriorityTokenPositions[i] = 3;
+					break;
 				case 4:
 				case 5:
 				case 6:
-					PriorityTokenPositions[i] = 3;
+					PriorityTokenPositions[i] = 4;
 					break;
 			}
 		}
@@ -179,8 +314,16 @@ public class Program
 
 	public static bool CheckSyntax(List<string> tokenizedList)
 	{
-		//TODO>add pair check
 		string[] keys = { "+", "^", "*", "/", "-" };
+		
+		for (int i = 0; i < keys.Length; i++)
+		{
+			if (tokenizedList[tokenizedList.Count-2] == keys[i] && tokenizedList[tokenizedList.Count-1] == "")
+			{
+				return false;
+			}
+		}
+
 		for (int i = 0; i < tokenizedList.Count - 1; i++)
 		{
 			for (int j = 0; j < keys.Length; j++)
@@ -194,5 +337,20 @@ public class Program
 		}
 		if (SearchForPairedTokens(tokenizedList)[tokenizedList.Count] == -1) return false;
 		return true;
+	}
+	public static void RemoveAndUpdate(double temp, ref List<string> tokenizedList, int i, ref int[] PriorityTokenPositions, ref int[] OperandPositions)
+	{
+		tokenizedList.RemoveRange(i - 1, 3);
+		tokenizedList.Insert(i - 1, Convert.ToString(temp));
+		PriorityTokenPositions = SearchForPriorityTokens(tokenizedList);
+		OperandPositions = SearchForOperands(tokenizedList);
+	}
+	public static void RemoveAndUpdateBrackets(ref List<string> tokenizedList, ref int[] PriorityTokenPositions, ref int[] OperandPositions, int pos1, int pos2)
+	{
+		tokenizedList.RemoveAt(pos2);
+		tokenizedList.RemoveAt(pos1);		
+		PriorityTokenPositions = SearchForPriorityTokens(tokenizedList);
+		OperandPositions = SearchForOperands(tokenizedList);
+
 	}
 }
